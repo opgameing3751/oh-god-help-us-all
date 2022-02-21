92accd1153bc755585e754a22b7978531b00cba7
opgameing3751 <jeepman3751@gmail.com>
2022-02-21 09:10:30 -0500

    save

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame, time, sys, os ,dotenv, random
 from pygame.locals import *
 from pygame import mixer
+from Player_1 import Player1
 fps = 0
 pygame.init()
-
+mousex = 0
+mousey = 0 
+start_screen = True
+mousepressed = False
 res = (1920, 1080)
 run = True
 wn = pygame.display.set_mode((res))
@@ -13,43 +17,20 @@ mainClock = pygame.time.Clock()
 BG = pygame.image.load("BG/bg.png")
 char = pygame.image.load("Char\char1.png").convert_alpha()
 pointer = pygame.image.load("pointer\pointer.png").convert_alpha()
-
+startBG = pygame.image.load('start screen\startBG.png').convert_alpha()
+start_button = pygame.image.load("start screen\start_button.png").convert_alpha()
+start_button_click = pygame.image.load("start screen\start_button_cliced.png").convert_alpha()
 #def
 def game_render():
-
-    wn.blit(BG, (0,0))
+    global mousex, mousey
+    mousex, mousey = player1.mouse
     wn.blit(fpsrender, (0,0))
-    wn.blit(pointer, (player.mouse))
+    wn.blit(pointer, (player1.mouse))
 
-#class
-class Player1:
-    
-    def __inti__(self, max_hp, damage):
-        self.max_hp = max_hp
-        self.hp = max_hp
-        self.damage = damage
-        self.image = char
-    def update(self):
-        self.mouse = pygame.mouse.get_pos()
+player1 = Player1(100,10)
 
-        keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_q]:
-            pygame.quit()
-            print("quit")
-        if keystate[pygame.K_0]:
-            mixer.music.load("SOUNDS\jam.mp3")
-            mixer.music.play(1)
 
-class Player2:
-    def __inti__(self,max_hp, damage):
-        self.max_hp = max_hp
-        self.hp = max_hp
-        self.damage = damage
-       
-
-player = Player1()
-"""female = Player1(100,10)
-male = Player2(100,10)   """ 
+ 
 while run:
     if pygame.mouse.get_visible():
                 pygame.mouse.set_visible(False)
@@ -59,12 +40,32 @@ while run:
             pygame.quit()
             running = 0
             print('game quit')
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        mousepressed = True
+    if event.type == pygame.MOUSEBUTTONUP:
+        mousepressed = False
+            
     
     #font renders
     font = pygame.font.Font(None,30)
     fpsrender = font.render(f'FPS {fps}',True,(255,255,255))
     
-    player.update()
+    if start_screen:
+        wn.blit(startBG, (0,0))
+
+        #start button
+        wn.blit(start_button, (895, 480))
+
+
+        if mousex > 895 and mousex < 1114 and mousey > 480 and mousey < 552 and mousepressed:
+            wn.blit(start_button_click, (895, 480))
+       
+
+    #if gamerunning1:
+        
+    #if gamerunning2:
+
+    player1.update()
     game_render()
     fps_s = time.time() - start
     fps1 = 1. / fps_s
