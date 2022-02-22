11bf4dc8f9c6516864e21bcc3130ed95d800ffc2
opgameing3751 <jeepman3751@gmail.com>
2022-02-22 08:32:23 -0500

    yes
    Co-authored-by: cdale2005 <cdale2005@users.noreply.github.com>

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ startBG = pygame.image.load('start screen\startBG.png').convert_alpha()
 start_button = pygame.image.load("start screen\start_button.png").convert_alpha()
 start_button_click = pygame.image.load("start screen\start_button_cliced.png").convert_alpha()
 player_sel = pygame.image.load('start screen\player sel.png').convert_alpha()
+button_box = pygame.image.load('start screen/button_box.png').convert_alpha()
+button_box_blue = pygame.image.load("start screen/button_box_blue.png")
 #def
 def game_render():
     global mousex, mousey
@@ -54,18 +56,28 @@ while run:
     
     if start_screen:
         wn.blit(startBG, (0,0))
-
+        
         #start button
         wn.blit(start_button, (895, 480))
-        if mousex > 895 and mousex < 1114 and mousey > 480 and mousey < 552:
+        if mousex > 895 and mousex < 1114 and mousey > 480 and mousey < 552 and start_screen:
             wn.blit(start_button_click, (895, 480))
 
-        if mousex > 895 and mousex < 1114 and mousey > 480 and mousey < 552 and mousepressed:
+        if mousex > 895 and mousex < 1114 and mousey > 480 and mousey < 552 and mousepressed and start_screen:
             wn.blit(start_button_click, (895, 480))
             time.sleep(0.5)
             playersel = True
+            start_screen = False
+
+
     if playersel:
         wn.blit(player_sel, (0,0))
+
+        #wn.blit(button_box, (113,192))
+        if mousex > 113 and mousex < 740 and mousey > 192 and mousey < 781 and playersel:
+            wn.blit(button_box, (113, 192))
+        if mousex > 1210 and mousex < 1837 and mousey > 178 and mousey < 767 and playersel:
+            wn.blit(button_box_blue, (1208, 180))
+       
 
     #if gamerunning1:
         
