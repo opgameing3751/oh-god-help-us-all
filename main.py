3cc109cb1c6555ac42ff0e778a76776faee808bf
opgameing3751 <jeepman3751@gmail.com>
2022-02-23 15:34:25 -0500

    game zone stage done

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import pygame, time, sys, os ,dotenv, random
 from pygame.locals import *
 from pygame import mixer
 from Player_1 import Player1
-fps = 0
 pygame.init()
+
+#var
+fps = 0
 mousex = 0
 mousey = 0 
 start_screen = True
@@ -13,8 +15,12 @@ player1_Girl = False
 player1_Boy = False
 player2_Boy = False
 player2_Girl = False
+zone_sel = False
 res = (1920, 1080)
 run = True
+mousepressed = 0
+
+
 wn = pygame.display.set_mode((res))
 mainClock = pygame.time.Clock()
 
@@ -28,6 +34,11 @@ start_button_click = pygame.image.load("start screen\start_button_cliced.png").c
 player_sel = pygame.image.load('start screen\player sel.png').convert_alpha()
 button_box = pygame.image.load('start screen/button_box.png').convert_alpha()
 button_box_blue = pygame.image.load("start screen/button_box_blue.png").convert_alpha()
+zone_sel_png = pygame.image.load("start screen\zone sel.png").convert()
+button_box_green = pygame.image.load('start screen/button_box_green.png').convert_alpha()
+button_rec_blue = pygame.image.load('start screen/button_rec_Blue.png').convert_alpha()
+button_rec_red = pygame.image.load('start screen/button_rec_Red.png').convert_alpha()
+
 #def
 def game_render():
     global mousex, mousey
@@ -40,6 +51,7 @@ player1 = Player1(100,10)
 
  
 while run:
+    print(mousepressed)
     if pygame.mouse.get_visible():
                 pygame.mouse.set_visible(False)
     start = time.time()
@@ -49,9 +61,9 @@ while run:
             running = 0
             print('game quit')
     if event.type == pygame.MOUSEBUTTONDOWN:
-        mousepressed = True
+        mousepressed = 1
     if event.type == pygame.MOUSEBUTTONUP:
-        mousepressed = False
+        mousepressed = 0
             
     
     #font renders
@@ -66,25 +78,59 @@ while run:
         if mousex > 895 and mousex < 1114 and mousey > 480 and mousey < 552 and start_screen:
             wn.blit(start_button_click, (895, 480))
 
-        if mousex > 895 and mousex < 1114 and mousey > 480 and mousey < 552 and mousepressed and start_screen:
+        if mousex > 895 and mousex < 1114 and mousey > 480 and mousey < 552 and mousepressed > 0 and start_screen:
             wn.blit(start_button_click, (895, 480))
             time.sleep(0.5)
             playersel = True
             start_screen = False
+            mousepressed = 0
 
 
     if playersel:
         wn.blit(player_sel, (0,0))
-
+        
         #wn.blit(button_box, (113,192))
-        if mousex > 113 and mousex < 740 and mousey > 192 and mousey < 781 and playersel:
-            wn.blit(button_box, (113, 192))
-            player1_Girl = True
-            player2_Boy = True
-        if mousex > 1210 and mousex < 1837 and mousey > 178 and mousey < 767 and playersel:
+
+        if mousex > 1210 and mousex < 1837 and mousey > 178 and mousey < 767:
             wn.blit(button_box_blue, (1208, 180))
-            player1_Boy = True
-            player2_Girl = True
+            if mousepressed > 0:
+                player1_Girl = False
+                player2_Boy = True
+                zone_sel = True
+                playersel = False
+                
+        if mousex > 113 and mousex < 740 and mousey > 192 and mousey < 781:
+            wn.blit(button_box, (113, 192))
+            if mousepressed > 0:
+                player1_Girl = True
+                player2_Boy = False
+                zone_sel = True
+                playersel = False
+                
+
+
+        
+      
+            
+
+    if zone_sel:
+        wn.blit(zone_sel_png, (0,0))
+
+        if mousex > 1150 and mousex < 1852 and mousey > 580 and mousey < 1000:
+                wn.blit(button_rec_red, (0, 0))
+                if mousepressed > 0:
+                    print('hi')
+    
+        if mousex > 658 and mousex < 1356 and mousey > 202 and mousey < 535:
+                wn.blit(button_rec_blue, (0, 0))
+                if mousepressed > 0:
+                    print('hi')
+
+        if mousex > 46 and mousex < 755 and mousey > 565 and mousey < 1015:
+                wn.blit(button_box_green, (0, 0))
+                if mousepressed > 0:
+                    print('hi')
+                
 
     #if gamerunning1:
         
