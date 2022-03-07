109e85b3af3709abf4360ce9dff656a56d8708e0
opgameing3751 <jeepman3751@gmail.com>
2022-03-07 09:04:36 -0500

    img

--- Player_2.py
+++ Player_2.py
@@ -8,7 +8,7 @@ class Player2:
         self.max_hp = max_hp
         self.hp = max_hp
         self.damage = damage
-        self.image = pygame.image.load('Char/testpose1.png')
+        #self.image = pygame.image.load('Char/testpose1.png')
         self.right2 = False
         self.left2 = False
         self.jump2 = False
@@ -17,32 +17,9 @@ class Player2:
         self.frame_index = 0
         self.action = 0
         self.update_time = pygame.time.get_ticks()
-# load idle images
-        temp_list = []
-        for i in range(1):
-            img = pygame.image.load(f'char/BOY/wank/0.png')
-            self.animation_list.append(temp_list)
-    
-        def update(self):
-            animation_cooldown = 100
-            # handle animation
-            # update image
-            self.image = self.animation_list[self.action][self.frame_index]
-            # check if enough time has passed since the last update
-            if pygame.time.get_ticks() - self.update_time > animation_cooldown:
-                self.update_time = pygame.time.get_ticks()
-                self.frame_index += 1
-            # if the animation has run out then reset back to the start
-            if self.frame_index >= len(self.animation_list[self.action]):
-                if self.action == 3:
-                    self.frame_index = len(self.animation_list[self.action]) - 1
-                else:
-                    self.idle()
-        def idle(self):
-            # set variables to idle animation
-            self.action = 0
-            self.frame_index = 0
-            self.update_time = pygame.time.get_ticks()
+
+        self.image = [pygame.image.load('Char\GIRL\wank\GW0.png'), pygame.image.load('Char\GIRL\wank\GW1.png'), pygame.image.load('Char\GIRL\wank\GW2.png'), pygame.image.load('Char\GIRL\wank\GW3.png')]  
+
 
     def update(self):
         
---
+++ README.txt
@@ -0,0 +1,7 @@
+it is a game that was made by people
+
+idk i mede this a 3am
+multiple sprites/
+splash start screen/
+score or timer
+game over screen
